8f25cd1291a9de5dec3d70f7ce3cc0af2aa7a237
brisher777 <brisher777@gmail.com>
2014-05-25 03:34:05 -0400

    testing VCS

--- mac_diff.py
+++ mac_diff.py
@@ -1,6 +1,8 @@
 import os
 import sqlite3
 
+#TODO: if a filename is already there, update it to the new mac times, but keep a dated version?
+
 ROOTDIR = os.path.abspath(os.sep)  # / or drive windows is installed on
 DATABASE = os.path.join(os.path.expanduser('~'), 'mac_times.db')
 
@@ -11,36 +13,40 @@ class DBContextManager():
 
         def __enter__(self):
             self._conn = sqlite3.connect(self._db_name)
+            self._conn.text_factory = str
             return self._conn
 
         def __exit__(self, type, value, traceback):
             if type:
                 self._conn.rollback()
+                print 'Exception: {}'.format(value)
             self._conn.close()
 
 def main():
-    for root, dirs, files in os.walk(ROOTDIR):
-        for file_name in files:
-            file_name = os.path.join(root, file_name)
-            if os.path.islink(file_name):
-                continue
-            try:
-                m_time = os.path.getmtime(file_name)
-                print m_time
-                a_time = os.path.getatime(file_name)
-                c_time = os.path.getctime(file_name)
-            except OSError as ex:
-                print ex
+    with DBContextManager(DATABASE) as conn:
+        cursor = conn.cursor()
+        for root, dirs, files in os.walk(ROOTDIR):
+            for file_name in files:
+                file_name = os.path.join(root, file_name)
+                if os.path.islink(file_name):
+                    continue
+                try:
+                    m_time = os.path.getmtime(file_name)
+                    a_time = os.path.getatime(file_name)
+                    c_time = os.path.getctime(file_name)
+                except OSError as ex:
+                    return 'Error {}'.format(ex.args[0])
+                cursor.execute('INSERT into files values (?,?,?,?)',
+                               (file_name, m_time, a_time, c_time))
+        conn.commit()
 
-
-def database():
+def create_database():
     if not os.path.exists(DATABASE):
         with DBContextManager(DATABASE) as conn:
             cursor = conn.cursor()
-            cursor.execute('''CREATE TABLE files(id INTEGER PRIMARY KEY, filename TEXT unique,
-                           m_time TEXT, a_time TEXT, c_time TEXT''')
+            cursor.execute('CREATE TABLE files(filename TEXT unique,'
+                           'm_time TEXT, a_time TEXT, c_time TEXT)')
             conn.commit()
 
-
-#main()
-database()
+create_database()
+main()
